7947b73f5a24c808e442d4ab435f683f651b191c
akanezero <zhubojian1998@gmail.com>
2020-06-17 23:40:50 +0800

    Site updated: 2020-06-17 23:40:50

--- js/fold_action.js
+++ js/fold_action.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-    $(document).on('click', '.fold_hider', function(){
-        $('>.fold', this.parentNode).slideToggle();
-        $('>:first', this).toggleClass('open');
-    });
-    //默认情况下折叠
-    $("div.fold").css("display","none");
-});
+// build time:Wed Jun 17 2020 23:40:35 GMT+0800 (GMT+08:00)
+$(document).ready(function(){$(document).on("click",".fold_hider",function(){$(">.fold",this.parentNode).slideToggle();$(">:first",this).toggleClass("open")});$("div.fold").css("display","none")});
+//rebuild by neat 
