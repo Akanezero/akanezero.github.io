aeef3cf36431bb01eaa085ca715a66bbf7a3046f
akanezero <zhubojian1998@gmail.com>
2020-06-28 02:37:17 +0800

    Site updated: 2020-06-28 02:37:17

--- js/fold_action.js
+++ js/fold_action.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 20 2020 16:23:12 GMT+0800 (GMT+08:00)
+// build time:Sun Jun 28 2020 02:37:13 GMT+0800 (GMT+08:00)
 $(document).ready(function(){$(document).on("click",".fold_hider",function(){$(">.fold",this.parentNode).slideToggle();$(">:first",this).toggleClass("open")});$("div.fold").css("display","none")});
 //rebuild by neat 
