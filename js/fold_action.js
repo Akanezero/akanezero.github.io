fc02eadfe87f2fe16447e14832f3ccc09a7a3cd8
akanezero <akanezero@akanezerodeMacBook-Pro.local>
2021-05-25 14:24:53 +0800

    Site updated: 2021-05-25 14:24:53

--- js/fold_action.js
+++ js/fold_action.js
@@ -1,3 +1,8 @@
-// build time:Sat Feb 20 2021 19:45:36 GMT+0800 (中国标准时间)
-$(document).ready(function(){$(document).on("click",".fold_hider",function(){$(">.fold",this.parentNode).slideToggle();$(">:first",this).toggleClass("open")});$("div.fold").css("display","none")});
-//rebuild by neat 
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    //默认情况下折叠
+    $("div.fold").css("display","none");
+});
